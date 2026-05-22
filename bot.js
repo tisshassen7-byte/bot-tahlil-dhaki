df9e3a18a5152a245abc69c488024d4a2fbb40a8
tisshassen7-byte <tisshassen7@gmail.com>
2026-05-22 10:32:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -166,3 +166,9 @@ bot.on('callback_query', async (query) => {
 });
 
 console.log('Bot real analysis running...');
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Bot alive');
+}).listen(process.env.PORT || 3000);
